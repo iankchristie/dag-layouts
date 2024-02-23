e7797fffae106f03a423bcaa5af4231fd8dcfc66
Ian Christie <iankchristie@gmail.com>
2024-02-23 14:23:59 -0700

    messing with imports again again again

--- state.js
+++ state.js
@@ -1,12 +1,12 @@
-import { updateZherebko } from "/zherebko.js";
-import { updateConstrained } from "/constrained.js";
-import { graph } from "/data.js";
-import { updateEdgeRouting } from "/edge-routing.js";
-import { updateSugiyama } from "/sugiyama.js";
-import { updateTangledTreeView } from "/tangled-tree/tangled-tree-view.js";
-import { updateUnconstrained } from "/unconstrained.js";
-import { updateGrid } from "/grid.js";
-import { update3D } from "/3d.js";
+import { updateZherebko } from "./zherebko.js";
+import { updateConstrained } from "./constrained.js";
+import { graph } from "./data.js";
+import { updateEdgeRouting } from "./edge-routing.js";
+import { updateSugiyama } from "./sugiyama.js";
+import { updateTangledTreeView } from "./tangled-tree/tangled-tree-view.js";
+import { updateUnconstrained } from "./unconstrained.js";
+import { updateGrid } from "./grid.js";
+import { update3D } from "./3d.js";
 
 export function UpdateState() {
   window.updateSelectedNode();

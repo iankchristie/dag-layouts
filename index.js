643b71245db3a4619d379361f5cd0947a8e4f6a9
Ian Christie <iankchristie@gmail.com>
2024-02-22 21:29:14 -0700

    functionality added

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import { addEdge, getChildren, getParents } from "./Graph";
+import typeahead from "typeahead-standalone";
+import {
+  addEdge,
+  deleteEdgeGraph,
+  deleteNodeGraph,
+  getAncestors,
+  getChildren,
+  getParents,
+} from "./Graph";
 import { graph, setGraph } from "./data";
 
 window.menuOptionChanged = (value) => {
@@ -13,7 +21,6 @@ const hideAll = function () {
   }
 };
 
-var selectedNode = null;
 window.updateSelectedNode = (nodeId) => {
   if (nodeId) {
     selectedNode = nodeId;
@@ -21,6 +28,16 @@ window.updateSelectedNode = (nodeId) => {
   document.getElementById("name").innerText = "Name: " + selectedNode;
   updateParents(selectedNode);
   updateChildren(selectedNode);
+  const allNodes = graph.nodes.map((n) => {
+    return n.id;
+  });
+  // for type ahead if i could get it to work
+  //   const ancestors = getAncestors(graph, selectedNode);
+  //   ancestors.add(selectedNode);
+  //   const reachableNodes = allNodes.filter((node) => !ancestors.has(node));
+  //   if (typeaheadInstance) {
+  //     typeaheadInstance.destroy();
+  //   }
 };
 
 const updateParents = function (nodeId) {
@@ -43,19 +60,42 @@ const updateChildren = function (nodeId) {
 
 const createListElement = function (name) {
   var li = document.createElement("li");
+  var childDiv = document.createElement("div");
+  var childName = document.createTextNode(name + "\t");
   li.style.cursor = "pointer";
-  li.addEventListener("click", () => {
+  childDiv.addEventListener("click", () => {
     window.updateSelectedNode(name);
   });
-  li.appendChild(document.createTextNode(name));
+  childDiv.appendChild(childName);
+  li.appendChild(childDiv);
+  const button = document.createElement("button");
+  button.appendChild(document.createTextNode("-"));
+  button.addEventListener("click", () => {
+    if (selectedNode === "Life") {
+      alert("Can't Delete Edge or else it would create multiple roots");
+      return;
+    }
+    const newGraph = deleteEdgeGraph(graph, { from: selectedNode, to: name });
+    setGraph(newGraph);
+  });
+
+  li.appendChild(button);
   return li;
 };
 
 window.updateSelectedNode("Life");
 
 window.addChild = () => {
-  console.log(selectedNode);
   const childName = document.getElementById("new-child").value;
   const newGraph = addEdge(graph, selectedNode, childName);
+  setGraph(newGraph);
+};
+
+window.deleteNode = () => {
+  if (selectedNode === "Life") {
+    alert("Can't Delete Root");
+    return;
+  }
+  const newGraph = deleteNodeGraph(graph, selectedNode);
   setGraph(newGraph);
 };
